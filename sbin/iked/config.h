34c263351c6f84a0092415da6b37f4e7746f2490
Christos Zoulas <christos@zoulas.com>
2018-07-02 16:08:58 -0400

    - Add samples build - regen configs.

--- sbin/iked/config.h
+++ sbin/iked/config.h
@@ -1,119 +1,134 @@
-/* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader.  */
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define to empty if the keyword does not work.  */
-/* #undef const */
+/* define if IPv6 advanced API is available */
+#define ADVAPI 1
 
-/* Define as the return type of signal handlers (int or void).  */
-#define RETSIGTYPE void
+/* recent versions of openssl declares argument with 'const' */
+#define BPP_const const
 
-/* Define to `unsigned' if <sys/types.h> doesn't define.  */
-/* #undef size_t */
+/* define to enable NAT Traversal support */
+#define ENABLE_NATT 1
 
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
-/* Define if you can safely include both <sys/time.h> and <time.h>.  */
-#define TIME_WITH_SYS_TIME 1
-
-/* The number of bytes in a long long.  */
-#define SIZEOF_LONG_LONG 8
-
-/* Define if you have the daemon function.  */
+/* Define to 1 if you have the `daemon' function. */
 #define HAVE_DAEMON 1
 
-/* Define if you have the getaddrinfo function.  */
+/* define if __func__ macro is available */
+#define HAVE_FUNC_MACRO 1
+
+/* Define to 1 if you have the `getaddrinfo' function. */
 #define HAVE_GETADDRINFO 1
 
-/* Define if you have the getnameinfo function.  */
+/* Define to 1 if you have the `getnameinfo' function. */
 #define HAVE_GETNAMEINFO 1
 
-/* Define if you have the getopt_long function.  */
+/* Define to 1 if you have the `getopt_long' function. */
 #define HAVE_GETOPT_LONG 1
 
-/* Define if you have the setprogname function.  */
-#define HAVE_SETPROGNAME 1
-
-/* Define if you have the timegm function.  */
-#define HAVE_TIMEGM 1
-
-/* Define if you have the <inttypes.h> header file.  */
+/* Define to 1 if you have the <inttypes.h> header file. */
 #define HAVE_INTTYPES_H 1
 
-/* Define if you have the <limits.h> header file.  */
+/* Define to 1 if you have the `pcap' library (-lpcap). */
+#define HAVE_LIBPCAP 1
+
+/* Define to 1 if you have the <limits.h> header file. */
 #define HAVE_LIMITS_H 1
 
-/* Define if you have the <net/pfkeyv2.h> header file.  */
-#define HAVE_NET_PFKEYV2_H 1
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
 
-/* Define if you have the <netdb.h> header file.  */
+/* Define to 1 if you have the <netdb.h> header file. */
 #define HAVE_NETDB_H 1
 
-/* Define if you have the <netinet/in.h> header file.  */
-#define HAVE_NETINET_IN_H 1
-
-/* Define if you have the <netinet6/ipsec.h> header file.  */
+/* Define to 1 if you have the <netinet6/ipsec.h> header file. */
 #define HAVE_NETINET6_IPSEC_H 1
 
-/* Define if you have the <netipsec/ipsec.h> header file.  */
+/* Define to 1 if you have the <netinet/in.h> header file. */
+#define HAVE_NETINET_IN_H 1
+
+/* Define to 1 if you have the <netipsec/ipsec.h> header file. */
 #define HAVE_NETIPSEC_IPSEC_H 1
 
-/* Define if you have the <openssl/aes.h> header file.  */
+/* Define to 1 if you have the <net/pfkeyv2.h> header file. */
+#define HAVE_NET_PFKEYV2_H 1
+
+/* Define to 1 if you have the <openssl/aes.h> header file. */
 #define HAVE_OPENSSL_AES_H 1
 
-/* Define if you have the <openssl/evp.h> header file.  */
+/* Define to 1 if you have the <openssl/evp.h> header file. */
 #define HAVE_OPENSSL_EVP_H 1
 
-/* Define if you have the <openssl/idea.h> header file.  */
+/* Define to 1 if you have the <openssl/idea.h> header file. */
 /* #undef HAVE_OPENSSL_IDEA_H */
 
-/* Define if you have the <openssl/opensslv.h> header file.  */
+/* Define to 1 if you have the <openssl/opensslv.h> header file. */
 #define HAVE_OPENSSL_OPENSSLV_H 1
 
-/* Define if you have the <openssl/pem.h> header file.  */
+/* Define to 1 if you have the <openssl/pem.h> header file. */
 #define HAVE_OPENSSL_PEM_H 1
 
-/* Define if you have the <openssl/pkcs12.h> header file.  */
+/* Define to 1 if you have the <openssl/pkcs12.h> header file. */
 #define HAVE_OPENSSL_PKCS12_H 1
 
-/* Define if you have the <openssl/rc5.h> header file.  */
+/* Define to 1 if you have the <openssl/rc5.h> header file. */
 /* #undef HAVE_OPENSSL_RC5_H */
 
-/* Define if you have the <openssl/rsa.h> header file.  */
+/* Define to 1 if you have the <openssl/rsa.h> header file. */
 #define HAVE_OPENSSL_RSA_H 1
 
-/* Define if you have the <openssl/x509.h> header file.  */
+/* define if openssl has SHA2 support */
+#define HAVE_OPENSSL_SHA2 1
+
+/* Define to 1 if you have the <openssl/x509.h> header file. */
 #define HAVE_OPENSSL_X509_H 1
 
-/* Define if you have the <stdarg.h> header file.  */
+/* define if struct sockaddr has sa_len field */
+#define HAVE_SA_LEN 1
+
+/* Define to 1 if you have the `setprogname' function. */
+#define HAVE_SETPROGNAME 1
+
+/* define if public key encryption is available */
+#define HAVE_SIGNING_C 1
+
+/* Define to 1 if you have the <stdarg.h> header file. */
 #define HAVE_STDARG_H 1
 
-/* Define if you have the <stddef.h> header file.  */
+/* Define to 1 if you have the <stddef.h> header file. */
 #define HAVE_STDDEF_H 1
 
-/* Define if you have the <stdint.h> header file.  */
+/* Define to 1 if you have the <stdint.h> header file. */
 #define HAVE_STDINT_H 1
 
-/* Define if you have the <stdlib.h> header file.  */
+/* Define to 1 if you have the <stdlib.h> header file. */
 #define HAVE_STDLIB_H 1
 
-/* Define if you have the <string.h> header file.  */
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
 #define HAVE_STRING_H 1
 
-/* Define if you have the <sys/param.h> header file.  */
+/* Define to 1 if you have the <sys/param.h> header file. */
 #define HAVE_SYS_PARAM_H 1
 
-/* Define if you have the <sys/socket.h> header file.  */
+/* Define to 1 if you have the <sys/socket.h> header file. */
 #define HAVE_SYS_SOCKET_H 1
 
-/* Define if you have the <sys/time.h> header file.  */
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
 #define HAVE_SYS_TIME_H 1
 
-/* Define if you have the <unistd.h> header file.  */
-#define HAVE_UNISTD_H 1
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
 
-/* Define if you have the pcap library (-lpcap).  */
-#define HAVE_LIBPCAP 1
+/* Define to 1 if you have the `timegm' function. */
+#define HAVE_TIMEGM 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
 
 /* path to the pid file */
 #define IKED_PID_FILE "/var/run/iked.pid"
@@ -124,48 +139,62 @@
 /* define to enable IKEv2 protocol support */
 #define IKEV2 1
 
-/* define to listen PF_ROUTE socket */
-#define WITH_RTSOCK 1
-
-/* define optaining CoA from netlink xfrm messages */
-/* #undef WITH_PARSECOA */
-
 /* window size of IPsec SA created by IKEv2 */
 /* #undef IKEV2_IPSEC_WINDOW_SIZE */
 
-/* define to enable NAT Traversal support */
-#define ENABLE_NATT 1
-
-/* define if __func__ macro is available */
-#define HAVE_FUNC_MACRO 1
-
-/* define if struct sockaddr has sa_len field */
-#define HAVE_SA_LEN 1
-
 #ifndef __NetBSD__
-/* Conditional by the build process */
 /* define if IPv6 is enabled */
+/* controlled by the build */
 #define INET6 1
 #endif
 
-/* define if IPv6 advanced API is available */
-#define ADVAPI 1
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
 
-/* define to use ADVAPI for glibc2 */ 
-/* #undef _GNU_SOURCE */
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* Define as the return type of signal handlers (`int' or `void'). */
+#define RETSIGTYPE void
+
+/* The size of `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG 8
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
 
 /* define to use OpenSSL ENGINE */
 #define WITH_OPENSSL_ENGINE 1
 
-/* define if public key encryption is available */
-#define HAVE_SIGNING_C 1
+/* define optaining CoA from netlink xfrm messages */
+/* #undef WITH_PARSECOA */
 
-/* recent versions of openssl declares argument with 'const' */
-#define BPP_const const
-
-/* define if openssl has SHA2 support */
-#define HAVE_OPENSSL_SHA2 1
+/* define to listen PF_ROUTE socket */
+/* #undef WITH_RTSOCK */
 
 /* define if SHA2 can be used */
 #define WITH_SHA2 1
 
+/* define for glibc to use ADVAPI */
+/* #undef _GNU_SOURCE */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
--- sbin/spmd/config.h
+++ sbin/spmd/config.h
@@ -1,101 +1,149 @@
-/* config.h.  Generated automatically by configure.  */
-/* config.h.in.  Generated automatically from configure.in by autoheader 2.13.  */
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
 
-/* Define to empty if the keyword does not work.  */
-/* #undef const */
-
-/* Define if you have the strftime function.  */
-#define HAVE_STRFTIME 1
-
-/* Define if you have <sys/wait.h> that is POSIX.1 compatible.  */
-#define HAVE_SYS_WAIT_H 1
-
-/* Define as __inline if that's what the C compiler calls it.  */
-/* #undef inline */
-
-/* Define to `int' if <sys/types.h> doesn't define.  */
-/* #undef pid_t */
-
-/* Define to `unsigned' if <sys/types.h> doesn't define.  */
-/* #undef size_t */
-
-/* Define if you have the ANSI C header files.  */
-#define STDC_HEADERS 1
-
-/* Define if you can safely include both <sys/time.h> and <time.h>.  */
-#define TIME_WITH_SYS_TIME 1
-
-/* Define if your <sys/time.h> declares struct tm.  */
-/* #undef TM_IN_SYS_TIME */
-
-/* Define if you have the getopt_long function.  */
-#define HAVE_GETOPT_LONG 1
-
-/* Define if you have the gettimeofday function.  */
-#define HAVE_GETTIMEOFDAY 1
-
-/* Define if you have the select function.  */
-#define HAVE_SELECT 1
-
-/* Define if you have the socket function.  */
-#define HAVE_SOCKET 1
-
-/* Define if you have the strdup function.  */
-#define HAVE_STRDUP 1
-
-/* Define if you have the strerror function.  */
-#define HAVE_STRERROR 1
-
-/* Define if you have the uname function.  */
-#define HAVE_UNAME 1
-
-/* Define if you have the strlcpy function.  */
-#define HAVE_STRLCPY 1
-
-/* Define if you have the strlcat function.  */
-#define HAVE_STRLCAT 1
-
-/* Define if you have the atoll function.  */
+/* Define to 1 if you have the `atoll' function. */
 #define HAVE_ATOLL 1
 
-/* Define if you have the <fcntl.h> header file.  */
+/* Define to 1 if you have the <fcntl.h> header file. */
 #define HAVE_FCNTL_H 1
 
-/* Define if you have the <getopt.h> header file.  */
+/* Define to 1 if you have the <getopt.h> header file. */
 #define HAVE_GETOPT_H 1
 
-/* Define if you have the <stdint.h> header file.  */
-#define HAVE_STDINT_H 1
+/* Define to 1 if you have the `getopt_long' function. */
+#define HAVE_GETOPT_LONG 1
 
-/* Define if you have the <inttypes.h> header file.  */
-#define HAVE_INTTYPES_H 1
+/* Define to 1 if you have the `gettimeofday' function. */
+#define HAVE_GETTIMEOFDAY 1
 
-/* Define if you have the <net/pfkeyv2.h> header file.  */
-#define HAVE_NET_PFKEYV2_H 1
-
-/* Define if you have the <sys/time.h> header file.  */
-#define HAVE_SYS_TIME_H 1
-
-/* Define if you have the <syslog.h> header file.  */
-#define HAVE_SYSLOG_H 1
-
-/* Define if you have the <unistd.h> header file.  */
-#define HAVE_UNISTD_H 1
-
-/* Define if you have <stdarg.h> header file. */
-#define HAVE_STDARG_H 1
-
-/* Define if struct sockaddr has sa_len field. */
-#define HAVE_SA_LEN 1
-
-/* Define if you have nsswitch.conf */
-#define HAVE_NSSWITCH_CONF 1
-
-/* Define if you have host.conf */
+/* use /etc/host.conf */
 /* #undef HAVE_HOST_CONF */
 
-/* Define if you have lookup line in your resolve.conf */
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the `crypto' library (-lcrypto). */
+#define HAVE_LIBCRYPTO 1
+
+/* Define to 1 if you have the `gnugetopt' library (-lgnugetopt). */
+/* #undef HAVE_LIBGNUGETOPT */
+
+/* use lookup in /etc/resolve.conf */
 /* #undef HAVE_LOOKUP_IN_RESOLV_CONF */
 
-/* Define if your kernel overwrites the current SPID by SPDUPDATE */
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <net/pfkeyv2.h> header file. */
+#define HAVE_NET_PFKEYV2_H 1
+
+/* use /etc/nsswitch.conf */
+#define HAVE_NSSWITCH_CONF 1
+
+/* define if struct sockaddr has sa_len field */
+#define HAVE_SA_LEN 1
+
+/* Define to 1 if you have the `select' function. */
+#define HAVE_SELECT 1
+
+/* Define to 1 if you have the `socket' function. */
+#define HAVE_SOCKET 1
+
+/* BSD does not keep SPID after spdupdate */
 #define HAVE_SPDUPDATE_BUG 1
+
+/* Define to 1 if you have the <stdarg.h> header file. */
+#define HAVE_STDARG_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the `strdup' function. */
+#define HAVE_STRDUP 1
+
+/* Define to 1 if you have the `strerror' function. */
+#define HAVE_STRERROR 1
+
+/* Define to 1 if you have the `strftime' function. */
+#define HAVE_STRFTIME 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the `strlcat' function. */
+#define HAVE_STRLCAT 1
+
+/* Define to 1 if you have the `strlcpy' function. */
+#define HAVE_STRLCPY 1
+
+/* Define to 1 if you have the <syslog.h> header file. */
+#define HAVE_SYSLOG_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/time.h> header file. */
+#define HAVE_SYS_TIME_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have <sys/wait.h> that is POSIX.1 compatible. */
+#define HAVE_SYS_WAIT_H 1
+
+/* Define to 1 if you have the `uname' function. */
+#define HAVE_UNAME 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME ""
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING ""
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME ""
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION ""
+
+/* The size of `long long', as computed by sizeof. */
+#define SIZEOF_LONG_LONG 8
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define to 1 if you can safely include both <sys/time.h> and <time.h>. */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define to 1 if your <sys/time.h> declares `struct tm'. */
+/* #undef TM_IN_SYS_TIME */
+
+/* Define to empty if `const' does not conform to ANSI C. */
+/* #undef const */
+
+/* Define to `__inline__' or `__inline' if that's what the C compiler
+   calls it, or to nothing if 'inline' is not supported under any name.  */
+#ifndef __cplusplus
+/* #undef inline */
+#endif
+
+/* Define to `int' if <sys/types.h> does not define. */
+/* #undef pid_t */
+
+/* Define to `unsigned int' if <sys/types.h> does not define. */
+/* #undef size_t */
